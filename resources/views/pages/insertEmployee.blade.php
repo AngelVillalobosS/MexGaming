059f1b1e4d9fbb8a83d20eb262a0b8aea35f99c8
AngelVillalobosS <angel.villalobos2269@uppuebla.edu.mx>
2025-03-06 06:45:28 -0600

    imgImplementation

--- resources/views/pages/insertEmployee.blade.php
+++ resources/views/pages/insertEmployee.blade.php
@@ -1,42 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{url('resources/css/inserts.css')}}">
-    <link rel="stylesheet" href="{{url('resources/css/tables.css')}}">
+    <link rel="stylesheet" href="{{asset('resources/css/inserts.css')}}">
+    <link rel="stylesheet" href="{{asset('resources/css/tables.css')}}">
     <title>Registrar Empleado</title>
 </head>
-<body>
-@include('components.navbar')
 
-<h1>Registro de Empleados</h1>
-<br>
-<table id="t1" class="tableRecords">
-    <tr>
-        <th>Nombre:</th>
-        <td><input type="text" class="txtCell" name="name" id="nameEmployee"></td>
-    </tr>
-    
-    <tr>
-        <th>Apellido Paterno:</th>
-        <td><input type="text" class="txtCell" name="lastnameEmployee" id="lastnameEmployee"></td>
-    </tr>
-    <tr>
-        <th>Apellido Materno:</th>
-        <td><input type="text" class="txtCell" name="" id=""></td>
-    </tr>
-    <tr>
-        <th>Nivel:</th>
-        <td>
-            <select name="level" id="levelEmployee" class="comboBox">
-                <option  value="J">Junior</option>
-                <option  value="SSR">Semi-Senior (SSR)</option>
-                <option  value="SR">Senior (SR)</option>
-            </select>
-        </td>
-    </tr>
-    <td id="picture">wasa</td>
-</table>
+<body>
+    @include('components.navbar')
+
+    <h1>Registro de Empleados</h1>
+    <br>
+    <table id="t1" class="tableRecords">
+        <tr>
+            <th>wwa</th>
+            <th>wwa</th>
+        </tr>
+        <tr>
+            <td>Nombre:</td>
+            <td><input type="text" class="txtCell" name="name" id="nameEmployee"></td>
+        </tr>
+
+        <tr>
+            <td>Apellido Paterno:</td>
+            <td><input type="text" class="txtCell" name="lastnameEmployee" id="lastnameEmployee"></td>
+        </tr>
+        <tr>
+            <td>Apellido Materno:</td>
+            <td><input type="text" class="txtCell" name="" id=""></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td>
+                <img src="{{ asset('resources/images/stock/dftl_employee.png') }}" alt="Imagen del empleado" class="imgEmployee"> <br>
+                <h1>wasa</h1>
+            </td>
+        </tr>
+        <tr>
+            <td>Nivel:</td>
+            <td>
+                <select name="level" id="levelEmployee" class="comboBox">
+                    <option value="J">Junior</option>
+                    <option value="SSR">Semi-Senior (SSR)</option>
+                    <option value="SR">Senior (SR)</option>
+                </select>
+            </td>
+        </tr>
+    </table>
 </body>
+
 </html>
